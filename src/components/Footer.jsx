ab508c1947ccc6451dffa23f009ea63ae773ba5f
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-03-05 15:07:55 +0300

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,31 @@ import React from "react";
 function Footer() {
   return (
     <footer className="py-3 px-6 flex items-center border-t border-gray-300 bg-gray-200 text-sm text-gray-500">
-      <div className="flex-1">&copy; 2022 Rainsoft. Allright Reserved</div>
+      <div className="flex-1">
+        &copy; {new Date().getFullYear()} Mobiklinic. All rights reserved.
+      </div>
       <div className="space-x-4 text-2xl text-gray-700 cursor-pointer">
-        <FontAwesomeIcon icon={faFacebookSquare}></FontAwesomeIcon>
-        <FontAwesomeIcon icon={faInstagramSquare}></FontAwesomeIcon>
-        <FontAwesomeIcon icon={faTwitterSquare}></FontAwesomeIcon>
+        <a
+          href="https://www.facebook.com/mobiklinicUganda"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFacebookSquare} />
+        </a>
+        <a
+          href="https://www.instagram.com/mobiklinic_foundation/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faInstagramSquare} />
+        </a>
+        <a
+          href="https://twitter.com/klinicuganda"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitterSquare} />
+        </a>
       </div>
     </footer>
   );
