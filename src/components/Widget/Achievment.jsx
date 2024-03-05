de3a5ae94e8d6db67f74996b33d622b758f909b9
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-03-05 14:23:23 +0300

    renal patients in the database

--- src/components/Widget/Achievment.jsx
+++ src/components/Widget/Achievment.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Achievement() {
+  const [totalPatients, setTotalPatients] = useState(0);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = `https://mk-be-strapi-production.up.railway.app/api/patients`;
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const patientsCount = data.data.length;
+        setTotalPatients(patientsCount);
+      })
+      .catch((error) => {
+        setError(error);
+        console.error("Error fetching patient data:", error);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
     <div className="widgetCard relative hidden md:flex col-span-2 px-6 py-4 text-center flex-col justify-between bg-cyan-700 text-slate-50">
       <div className="font-semibold text-slate-800 bg-white max-w-fit mx-auto pt-5 pb-2 px-6 absolute -top-3 rounded-lg left-1/2 -translate-x-1/2 whitespace-nowrap">
-        Laba (YtD)
+        Patients Treated{" "}
       </div>
       <div className="font-semibold m-auto pt-4">
-        <span className="text-lg lg:text-[80px]">100</span>.
-        <span className="text-[14px]">01</span>%
+        <span className="text-lg lg:text-[80px]">{totalPatients}</span>
+        <span className="text-[14px]">patients</span>
       </div>
-      <p className="text-sm font-semibold">IDR 1.800 M Year to Date</p>
-      <p className="text-xs">21 Mei 23 to GS 2023</p>
+      {/* <p className="text-sm font-semibold">Pati</p> */}
     </div>
   );
 }
