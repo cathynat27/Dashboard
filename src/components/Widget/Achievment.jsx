ba0c26c1557e5c938a4a97d82f045f20c03d6672
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-03-07 11:34:46 +0300

    total patients

--- src/components/Widget/Achievment.jsx
+++ src/components/Widget/Achievment.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 function Achievement() {
   const [totalPatients, setTotalPatients] = useState(0);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     try {
@@ -10,14 +11,16 @@ function Achievement() {
         `https://mk-be-strapi-production.up.railway.app/api/all-patients`
       );
       const data = await response.json();
-      setTotalPatients(data.length);
+      const allPatients = data.map((user) => user.patients).flat();
 
+      setTotalPatients(allPatients.length);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
       setError(error);
+      setLoading(false);
     }
   };
-  
 
   useEffect(() => {
     fetchData();
@@ -29,8 +32,9 @@ function Achievement() {
   return (
     <div className="widgetCard relative hidden md:flex col-span-2 px-6 py-4 text-center flex-col justify-between bg-cyan-700 text-slate-50">
       <div className="font-semibold text-slate-800 bg-white max-w-fit mx-auto pt-5 pb-2 px-6 absolute -top-3 rounded-lg left-1/2 -translate-x-1/2 whitespace-nowrap">
-        Patients Treated{" "}
+        Patients Treated
       </div>
+
       <div className="font-semibold m-auto pt-4">
         <span className="text-lg lg:text-[80px]">{totalPatients}</span>
         <span className="text-[14px]">patients</span>
