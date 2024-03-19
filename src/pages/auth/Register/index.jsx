c5b6320cbd0aab77f8ca0fadaa112f8120bb299d
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2024-03-19 17:02:55 +0300

    register account page

--- src/pages/auth/Register/index.jsx
+++ src/pages/auth/Register/index.jsx
@@ -1,26 +1,67 @@
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faLock,
+  faUser,
+  faEye,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext";
 
 function RegisterIndex() {
   const navigate = useNavigate();
   const [error, setError] = useState(null);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const { setUserRole, updateUserNames } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+  const [signupCredentials, setSignUpCredentials] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
   const [loading, setLoading] = useState(false);
-  const handleSubmit = () => {
-    setError(false);
-    console.log(email);
-    console.log(name);
-    console.log(password);
-    console.log(confirmPassword);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     setLoading(true);
-    navigate("/");
+
+    try {
+      const response = await fetch(
+        "https://mk-be-strapi-production.up.railway.app/api/auth/local/register",
+        {
+          method: "POST",
+          body: JSON.stringify(signupCredentials),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setUserRole(data.role);
+        navigate("/auth/login");
+      } else {
+        setError("Invalid username or password");
+      }
+    } catch (error) {
+      console.error("Sign Up error:", error);
+      setError("An error occurred while logging in. Please try again later.");
+    }
+
+    setLoading(false);
   };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   const registerImage =
     "https://edp.raincode.my.id/static/media/login.cc0578413db10119a7ff.png";
   return (
@@ -39,9 +80,6 @@ function RegisterIndex() {
                 <h1 className="font-semibold text-2xl pb-2">
                   Register an Account
                 </h1>
-                <span className="text-sm">
-                  Free access to EDP Online services
-                </span>
               </div>
             </div>
           </div>
@@ -50,14 +88,14 @@ function RegisterIndex() {
             <div className="loginWrapper flex flex-col w-full lg:px-36 md:px-8 px-8 md:py-8">
               {/* Login Header Text */}
               <div className="hidden md:block font-medium self-center text-xl sm:text-3xl text-gray-800">
-                Buatkan Akun
+                Sign Up{" "}
               </div>
 
               {/* Sparator */}
               <div className="hidden md:block relative mt-10 h-px bg-gray-300">
                 <div className="absolute left-0 top-0 flex justify-center w-full -mt-2">
                   <span className="bg-white px-4 text-xs text-gray-500 uppercase">
-                    Daftarkan akun Anda sekarang, Gratis!
+                    Please Fill in your Details{" "}
                   </span>
                 </div>
               </div>
@@ -69,6 +107,62 @@ function RegisterIndex() {
               {/* Register Form */}
               <div className="md:mt-10 mt-4">
                 <form onSubmit={handleSubmit}>
+                  <div className="flex flex-col mb-3">
+                    <div className="relative">
+                      <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </div>
+
+                      <input
+                        id="firstName"
+                        type="text"
+                        name="firstName"
+                        onChange={(e) =>
+                          setSignUpCredentials({
+                            ...signupCredentials,
+                            firstName: e.target.value,
+                          })
+                        }
+                        value={signupCredentials.firstName}
+                        className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full md:py-2 py-3 focus:outline-none focus:border-sky-400"
+                        placeholder="Enter First Name"
+                      />
+                    </div>
+                    {error?.firstName && (
+                      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                        {error.firstName[0]}
+                      </span>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col mb-3">
+                    <div className="relative">
+                      <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                      </div>
+
+                      <input
+                        id="lastName"
+                        type="text"
+                        name="lastName"
+                        onChange={(e) =>
+                          setSignUpCredentials({
+                            ...signupCredentials,
+                            lastName: e.target.value,
+                          })
+                        }
+                        value={signupCredentials.lastName}
+                        className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full md:py-2 py-3 focus:outline-none focus:border-sky-400"
+                        placeholder="Enter Last Name"
+                      />
+                    </div>
+                    {error?.lastName && (
+                      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                        {error.lastName[0]}
+                      </span>
+                    )}
+                  </div>
+
                   {/* Username */}
                   <div className="flex flex-col mb-3">
                     <div className="relative">
@@ -80,9 +174,15 @@ function RegisterIndex() {
                         id="email"
                         type="text"
                         name="email"
-                        onChange={(e) => setEmail(e.target.value)}
+                        onChange={(e) =>
+                          setSignUpCredentials({
+                            ...signupCredentials,
+                            email: e.target.value,
+                          })
+                        }
+                        value={signupCredentials.email}
                         className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full md:py-2 py-3 focus:outline-none focus:border-sky-400"
-                        placeholder="E-Mail Address"
+                        placeholder="Enter E-Mail Address"
                       />
                     </div>
                     {error?.email && (
@@ -103,9 +203,15 @@ function RegisterIndex() {
                         id="name"
                         type="text"
                         name="name"
-                        onChange={(e) => setName(e.target.value)}
+                        onChange={(e) =>
+                          setSignUpCredentials({
+                            ...signupCredentials,
+                            username: e.target.value,
+                          })
+                        }
+                        value={signupCredentials.username}
                         className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full md:py-2 py-3 focus:outline-none focus:border-sky-400"
-                        placeholder="Nama Lengkap"
+                        placeholder="Enter unique user name "
                       />
                     </div>
                     {error?.name && (
@@ -124,11 +230,22 @@ function RegisterIndex() {
 
                       <input
                         id="password"
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         name="password"
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={(e) =>
+                          setSignUpCredentials({
+                            ...signupCredentials,
+                            password: e.target.value,
+                          })
+                        }
+                        value={signupCredentials.password}
                         className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full md:py-2 py-3 focus:outline-none focus:border-sky-400"
                         placeholder="Password"
+                      />
+                      <FontAwesomeIcon
+                        icon={showPassword ? faEyeSlash : faEye}
+                        className="absolute top-0 right-0 mt-2 mr-4 text-gray-500 cursor-pointer"
+                        onClick={togglePasswordVisibility}
                       />
                     </div>
                     {error?.password && (
@@ -136,44 +253,6 @@ function RegisterIndex() {
                         {error.password[0]}
                       </span>
                     )}
-                  </div>
-
-                  {/* Konfirmasi Password */}
-                  <div className="flex flex-col mb-6">
-                    <div className="relative">
-                      <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
-                        <FontAwesomeIcon icon={faLock} />
-                      </div>
-
-                      <input
-                        id="confirm_password"
-                        type="password"
-                        name="confirm_password"
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        className="text-sm placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full md:py-2 py-3 focus:outline-none focus:border-sky-400"
-                        placeholder="Konfirmasi Password"
-                      />
-                    </div>
-                    {error?.confirm_password && (
-                      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
-                        {error.confirm_password[0]}
-                      </span>
-                    )}
-                  </div>
-
-                  {/* Forgot Password Link */}
-                  <div className="flex items-center mb-6 -mt-2 md:-mt-4">
-                    <div className="flex ml-auto">
-                      <Link
-                        to=""
-                        onClick={(e) => {
-                          e.preventDefault();
-                        }}
-                        className="inline-flex font-semibold text-xs sm:text-sm text-sky-500 hover:text-sky-700"
-                      >
-                        Lupa password?
-                      </Link>
-                    </div>
                   </div>
 
                   {/* Button Register */}
@@ -191,38 +270,6 @@ function RegisterIndex() {
                 </form>
               </div>
 
-              {/* Sparator */}
-              <div className="relative mt-6 h-px bg-gray-300">
-                <div className="absolute left-0 top-0 flex justify-center w-full -mt-2">
-                  <span className="bg-white px-4 text-xs text-gray-500 uppercase">
-                    OR
-                  </span>
-                </div>
-              </div>
-
-              {/* Social Button */}
-              <div className="flex justify-between w-full mt-6">
-                <button
-                  disabled={loading}
-                  type="submit"
-                  className="flex items-center justify-center focus:outline-none text-slate-500 text-sm bg-slate-200 rounded-lg md:rounded md:py-2 px-3 py-3 w-full transition duration-150 ease-in"
-                >
-                  <FontAwesomeIcon icon={faGoogle} />
-                  <span className="mr-2 flex-1">Login with Google</span>
-                </button>
-              </div>
-              <div className="flex justify-between w-full mt-2">
-                <button
-                  disabled={loading}
-                  type="submit"
-                  className="flex items-center justify-center focus:outline-none text-slate-500 text-sm bg-slate-200 rounded-lg md:rounded md:py-2 px-3 py-3 w-full transition duration-150 ease-in"
-                >
-                  <FontAwesomeIcon icon={faFacebook} />
-                  <span className="mr-2 flex-1">Login with Facebook</span>
-                </button>
-              </div>
-              {/* End Social Button */}
-
               {/* Register Link */}
               <div className="flex justify-center items-center my-6 md:mb-0">
                 <Link
@@ -231,7 +278,7 @@ function RegisterIndex() {
                 >
                   <span>
                     <svg
-                      className="h-6 w-6"
+                      className="h-4 w-4"
                       fill="none"
                       strokeLinecap="round"
                       strokeLinejoin="round"
@@ -242,7 +289,7 @@ function RegisterIndex() {
                       <path d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path>
                     </svg>
                   </span>
-                  <span className="ml-2">Sudah punya akun?</span>
+                  <span className="ml-2">Have An Account?</span>
                 </Link>
               </div>
               {/* End Register Link */}
