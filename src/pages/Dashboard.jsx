1b0ac092c102132820d38f8f63199c4c3762b6f1
Arafat Magezi <arafats144@gmail.com>
2024-10-14 14:13:12 +0300

    Implement the NAFS Numbers omn the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -381,7 +381,7 @@ function Dashboard() {
           >
             NAFS Project
           </h1>
-          {nafsProjectData.length > 0 && (
+          {showNafs && nafsProjectData.length > 0 && (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {nafsProjectData.map((data, index) => (
                 <div key={index} className="bg-gray-800 p-4 rounded-lg">
